683ecfee5a41f1562f7d89846cf8b66cf01a61b2
AeroztaR <jorguinhos89@gmail.com>
2018-03-05 05:24:44 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+    let wrapper = document.querySelector('.wrapper'),
+        topLayer = document.querySelector('.top'),
+        handle = wrapper.querySelector('.handle'),
+        skew = 0,
+        delta = 0;
+
+    if(wrapper.className.indexOf('skewed') != -1) {
+        skew = 1000;
+    }
+        
+     wrapper.addEventListener('mousemove', (e) => {
+        delta = (e.clientX - window.innerWidth / 2) * 0.5;
+        
+        handle.style.left = e.clientX + delta + 'px';
+
+        topLayer.style.width = e.clientX + skew + delta + 'px';
+    }); 
+});
+
